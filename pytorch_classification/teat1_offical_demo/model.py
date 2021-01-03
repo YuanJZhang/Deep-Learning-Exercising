fab7a4210c59996825fee7bbbf6f6b100d57aeb6
YuanJZhang <18302403034@163.com>
2021-01-03 10:17:24 +0800

    first push

---
+++ pytorch_classification/teat1_offical_demo/model.py
@@ -0,0 +1,31 @@
+import torch.nn as nn   # 导入pytorch模型 torch
+import torch.nn.functional as F
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 5)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(32*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))    # input(3, 32, 32) output(16, 28, 28)
+        x = self.pool1(x)            # output(16, 14, 14)
+        x = F.relu(self.conv2(x))    # output(32, 10, 10)
+        x = self.pool2(x)            # output(32, 5, 5)
+        x = x.view(-1, 32*5*5)       # output(32*5*5)  全连接层输入是一维向量 必须对数据展平 这里的-1是第一个维度(batch)是自动推理的（pytorch tensor（张量）他的通道排序为[batch channel height width] ） 指batch自动计算
+        x = F.relu(self.fc1(x))      # output(120)
+        x = F.relu(self.fc2(x))      # output(84)
+        x = self.fc3(x)              # output(10)
+        return x
+
+import torch
+input1 = torch.rand([32,3,32,32])
+model=LeNet()
+print(model)
+output =model(input1)
